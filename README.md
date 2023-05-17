69ea8824e7eb2c660b272be06f62cafb7e13ece7
melisaapaz <105674450+melisaapaz@users.noreply.github.com>
2023-05-16 22:53:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-qtzaj6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fdt3s1)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { SharedModule } from "./shared/shared.module";
+import { PagesModule } from "./pages/pages.module";
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, SharedModule, PagesModule  ],
+  declarations: [ AppComponent, HelloComponent ],
+  providers: [],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
